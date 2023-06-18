d9d0eccef5c2f066a8d883c3ddf47b51736d624f
ngel rojas <aomarrojasp@gmail.com>
2023-06-18 18:45:38 -0300

    add: adding transformar

--- app/check_in/views.py
+++ app/check_in/views.py
@@ -59,6 +59,9 @@ class JSONFileView(viewsets.ViewSet):
                         Key=destination_file_name,
                         Body=response_data_str
                     )
+                    # convert json to cnab750
+                    # cnab750_data = self.convert_to_cnab750(response_data_str)
+                    # print(cnab750_data)
                 except ClientError as e:
                     print(f"Error uploading file {destination_file_name}: {e.response['Error']['Message']}")
 
@@ -70,3 +73,46 @@ class JSONFileView(viewsets.ViewSet):
         await asyncio.gather(*tasks)
 
         return file_list
+
+    def convert_to_cnab750(self, data):
+        # data = {
+        #     "HEADER": {...},  # JSON data
+        #     ...
+        #         "CODIGOS_DE_ERROR": [...]  # JSON data
+        # }
+
+        cnab750_data = ""
+
+        # Add HEADER data
+        header_data = data["HEADER"]
+        cnab750_data += "|".join(header_data.values()) + "\r\n"
+
+        # Add DETALHE data
+        detalhe_data = data["DETALHE"][0]
+        cnab750_data += "|".join(detalhe_data.values()) + "\r\n"
+
+        # Add REGISTRO_DETALHE_INFORMACOES_ADICIONAIS data
+        informacoes_adicionais = detalhe_data["REGISTRO_DETALHE_INFORMACOES_ADICIONAIS"][0]
+        cnab750_data += "|".join(informacoes_adicionais.values()) + "\r\n"
+
+        # Add EMV data
+        emv_data = data["EMV"]
+        cnab750_data += "|".join(emv_data.values()) + "\r\n"
+
+        # Add REGISTRO_DE_RECEBIMENTO data
+        registro_recebimento = emv_data["REGISTRO_DE_RECEBIMENTO"][0]
+        cnab750_data += "|".join(registro_recebimento.values()) + "\r\n"
+
+        # Add TRAILER data
+        trailer_data = data["TRAILER"]
+        cnab750_data += "|".join(trailer_data.values()) + "\r\n"
+
+        # Add CODIGOS_DE_ERROR data
+        cnab750_data += "|".join(data["CODIGOS_DE_ERROR"]) + "\r\n"
+
+        # Return the CNAB750 file as a response
+        # response = HttpResponse(content_type="text/plain")
+        # response["Content-Disposition"] = "attachment; filename=cnab750.rem"
+        # response.write(cnab750_data)
+
+        return cnab750_data
