8f1f439de42276a4245c17c1a666501c2a0e72f0
ngel rojas <aomarrojasp@gmail.com>
2023-06-20 19:49:12 -0300

    add: adding loggin for cloud watch

--- app/check_in/views.py
+++ app/check_in/views.py
@@ -3,6 +3,7 @@ import boto3
 import json
 import os
 import asyncio
+import logging
 from botocore.exceptions import ClientError
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
@@ -14,6 +15,7 @@ class JSONFileView(viewsets.ViewSet):
     queryset = None
 
     def get(self, request):
+        logging.info("into the get method")
         source_bucket_name = os.environ.get("AWS_STORAGE_BUCKET_NAME")
         destination_bucket_name = "citi-qr-response"
 
@@ -24,12 +26,14 @@ class JSONFileView(viewsets.ViewSet):
         return Response(file_list)
 
     async def process_files(self, source_bucket_name, destination_bucket_name):
+        logging.info("into the process_files method")
         s3 = boto3.resource('s3')
         source_bucket = s3.Bucket(source_bucket_name)
         destination_bucket = s3.Bucket(destination_bucket_name)
         file_list = []
 
         async def files(obj):
+            logging.info("into the files method async function")
             if obj.key.endswith('.json'):
                 file_data = obj.get()['Body'].read()
                 file_data = file_data.decode('utf-8')
@@ -59,9 +63,6 @@ class JSONFileView(viewsets.ViewSet):
                         Key=destination_file_name,
                         Body=response_data_str
                     )
-                    # convert json to cnab750
-                    # cnab750_data = self.convert_to_cnab750(response_data_str)
-                    # print(cnab750_data)
                 except ClientError as e:
                     print(f"Error uploading file {destination_file_name}: {e.response['Error']['Message']}")
 
@@ -73,46 +74,3 @@ class JSONFileView(viewsets.ViewSet):
         await asyncio.gather(*tasks)
 
         return file_list
-
-    def convert_to_cnab750(self, data):
-        # data = {
-        #     "HEADER": {...},  # JSON data
-        #     ...
-        #         "CODIGOS_DE_ERROR": [...]  # JSON data
-        # }
-
-        cnab750_data = ""
-
-        # Add HEADER data
-        header_data = data["HEADER"]
-        cnab750_data += "|".join(header_data.values()) + "\r\n"
-
-        # Add DETALHE data
-        detalhe_data = data["DETALHE"][0]
-        cnab750_data += "|".join(detalhe_data.values()) + "\r\n"
-
-        # Add REGISTRO_DETALHE_INFORMACOES_ADICIONAIS data
-        informacoes_adicionais = detalhe_data["REGISTRO_DETALHE_INFORMACOES_ADICIONAIS"][0]
-        cnab750_data += "|".join(informacoes_adicionais.values()) + "\r\n"
-
-        # Add EMV data
-        emv_data = data["EMV"]
-        cnab750_data += "|".join(emv_data.values()) + "\r\n"
-
-        # Add REGISTRO_DE_RECEBIMENTO data
-        registro_recebimento = emv_data["REGISTRO_DE_RECEBIMENTO"][0]
-        cnab750_data += "|".join(registro_recebimento.values()) + "\r\n"
-
-        # Add TRAILER data
-        trailer_data = data["TRAILER"]
-        cnab750_data += "|".join(trailer_data.values()) + "\r\n"
-
-        # Add CODIGOS_DE_ERROR data
-        cnab750_data += "|".join(data["CODIGOS_DE_ERROR"]) + "\r\n"
-
-        # Return the CNAB750 file as a response
-        # response = HttpResponse(content_type="text/plain")
-        # response["Content-Disposition"] = "attachment; filename=cnab750.rem"
-        # response.write(cnab750_data)
-
-        return cnab750_data
